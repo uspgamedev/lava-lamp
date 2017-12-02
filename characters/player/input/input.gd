4da4f02adcf9541e9dd77cc807adbfb22608835e
Yan Couto <yancouto@gmail.com>
2017-12-02 16:43:17 -0200

    Letting go of diagonal direction

--- characters/player/input/input.gd
+++ characters/player/input/input.gd
@@ -10,10 +10,15 @@ signal press_direction(dir)
 signal press_action(act)
 signal press_quit
 
+var cur = 0
+# Last time this direction was held
+var last_dir_hold = []
 
 func _ready():
 	set_fixed_process(true)
 	set_process_input(true)
+	for i in range(4):
+		last_dir_hold.append(-10)
 
 func _input(event):
 	var dir = self._get_direction(event)
@@ -23,6 +28,7 @@ func _input(event):
 	if _get_quit(event): emit_signal('press_quit')
 
 func _fixed_process(delta):
+	cur += delta
 	var dir = self._get_direction(Input)
 	var act = self._get_action(Input)
 	var look_dir = self._get_look_direction(Input)
@@ -39,26 +45,43 @@ func _get_action(e):
 	return act
 
 func _get_look_direction(e):
+	var i = 0
+	for cmd in ['look_up', 'look_down', 'look_right', 'look_left']:
+		if e.is_action_pressed(cmd):
+			last_dir_hold[i] = cur
+		i += 1
+	# Only considers a key is not pressed if it has not been pressed for threshold seconds
+	var threshold = .1
+	# alx = is actually pressing the direction x
+	# lx = has pressed the direction x in the last threshold secs
+	var alu = last_dir_hold[0] == cur
+	var lu = last_dir_hold[0] >= cur - threshold
+	var ald = last_dir_hold[1] == cur
+	var ld = last_dir_hold[1] >= cur - threshold
+	var alr = last_dir_hold[2] == cur
+	var lr = last_dir_hold[2] >= cur - threshold
+	var all = last_dir_hold[3] == cur
+	var ll = last_dir_hold[3] >= cur - threshold
 	var dir = -1
-	if e.is_action_pressed('look_up') and not e.is_action_pressed('look_down'):
+	if alu and not ld:
 		dir = DIR.UP
-	elif e.is_action_pressed('look_down') and not e.is_action_pressed('look_up'):
+	elif ald and not lu:
 		dir = DIR.DOWN
-	if e.is_action_pressed('look_right') and not e.is_action_pressed('look_left'):
+	if alr and not ll:
 		dir = DIR.RIGHT
-	elif e.is_action_pressed('look_left') and not e.is_action_pressed('look_right'):
+	elif all and not lr:
 		dir = DIR.LEFT
-	if e.is_action_pressed('look_up') and e.is_action_pressed('look_right') \
-		and not e.is_action_pressed('look_down') and not e.is_action_pressed('look_left'):
+	if lu and lr \
+		and not ld and not ll:
 		dir = DIR.UP_RIGHT
-	elif e.is_action_pressed('look_down') and e.is_action_pressed('look_left') \
-		and not e.is_action_pressed('look_up') and not e.is_action_pressed('look_right'):
+	elif ld and ll \
+		and not lu and not lr:
 		dir = DIR.DOWN_LEFT
-	if e.is_action_pressed('look_down') and e.is_action_pressed('look_right') \
-		and not e.is_action_pressed('look_up') and not e.is_action_pressed('look_left'):
+	if ld and lr \
+		and not lu and not ll:
 		dir = DIR.DOWN_RIGHT
-	elif e.is_action_pressed('look_up') and e.is_action_pressed('look_left') \
-		and not e.is_action_pressed('look_down') and not e.is_action_pressed('look_right'):
+	elif lu and ll \
+		and not ld and not lr:
 		dir = DIR.UP_LEFT
 	return dir
 
