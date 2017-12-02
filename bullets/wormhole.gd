844333e4e5808bba5b28793c3b3051066ef17bab
IceMage144 <jgabriel.basi@hotmail.com>
2017-12-02 16:43:54 -0200

    Make wormholes spawn at a random valid location

--- bullets/wormhole.gd
+++ bullets/wormhole.gd
@@ -1,9 +1,17 @@
 extends 'res://characters/body_fixed_speed.gd'
 
+var brother = false
+
+func set_brother(b):
+	brother = b
+	get_node("Sprite").set_modulate(Color(0.5, 0.5, 1))
+
 func _on_Area2D_area_enter(area):
-	if area.is_in_group('enemy_area') or area.is_in_group('player_area'):
+	if (area.is_in_group('walls')):
+		set_pos(randi()%1280, randi()%720)
+	if (area.is_in_group('enemy_area') or area.is_in_group('player_area')) and brother:
 		var other = area.get_parent()
-		var relative_pos = other.get_pos() - get_pos()
-		var wh_list = get_tree().get_nodes_in_group("wormholes")
-		var brother = wh_list[randi()%wh_list.size()]
-		other.set_pos(brother.get_pos() - relative_pos)
+		var mem = other.get_pos()
+		other.set_pos(brother.get_pos())
+		if (other.is_colliding()):
+			other.set_pos(mem)
