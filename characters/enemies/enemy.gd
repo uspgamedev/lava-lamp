1dad0be019198f6c050e5a8553e59906701f5cd0
Yan Couto <yancouto@gmail.com>
2017-12-02 10:51:10 -0200

    Don't crash when player dies

--- characters/enemies/enemy.gd
+++ characters/enemies/enemy.gd
@@ -1,6 +1,6 @@
 extends 'res://characters/body.gd'
 
-var s = 3
+var s = 1
 
 var player
 var cd_timer
@@ -13,13 +13,11 @@ func _ready():
 func _fixed_process(delta):
 	if cd_timer.get_time_left() == 0:
 		var vec = player.get_pos() - get_pos()
-		move(s*vec.normalized())
+		move(s * vec.normalized())
 
 func _on_Area2D_area_enter( area ):
 	if area.is_in_group("player_area"):
 		var vec = player.get_pos() - get_pos()
-		player.speed += 1000*s*vec.normalized()
-		player.damage += 1
+		player.speed += 3000 * s * vec.normalized()
+		player.deal_damage(1)
 		cd_timer.start()
-		if (player.damage >= player.hp):
-				player.queue_free()
