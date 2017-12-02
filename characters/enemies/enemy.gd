327f2a9082dbabcd30678b49710115baa3b0b1a3
IceMage144 <jgabriel.basi@hotmail.com>
2017-12-02 04:44:09 -0200

    Add knockback

--- characters/enemies/enemy.gd
+++ characters/enemies/enemy.gd
@@ -1,12 +1,22 @@
-extends KinematicBody2D
+extends 'res://characters/body.gd'
 
-var speed = 3
+var s = 3
+
 var player
+var cd_timer
 
 func _ready():
 	player = get_node("../Player")
+	cd_timer = get_node("Cooldown")
 	set_fixed_process(true)
 	
 func _fixed_process(delta):
-	var vec = player.get_pos() - get_pos()
-	move(speed*vec/vec.length())
+	if cd_timer.get_time_left() == 0:
+		var vec = player.get_pos() - get_pos()
+		move(s*vec/vec.length())
+
+func _on_Area2D_area_enter( area ):
+	if area.is_in_group("player_area"):
+		var vec = player.get_pos() - get_pos()
+		player.speed += 10*s*vec
+		cd_timer.start()
