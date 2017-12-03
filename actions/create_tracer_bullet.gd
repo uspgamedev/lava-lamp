d02fd8169db9a7b6bb06efd161c80e0f5d8afc50
Kazuo256 <kazuo256@gmail.com>
2017-12-03 12:31:30 -0200

    GIGAAAAAAA

--- actions/create_tracer_bullet.gd
+++ actions/create_tracer_bullet.gd
@@ -4,11 +4,10 @@ func _init():
 	cooldown_time = .4
 
 func activate(action_handler):
-	self.icon = preload("res://bullets/simple_bullet_sprite.tscn")
-	var SimpleBullet = preload('res://bullets/simple_bullet.tscn')
-	var b = SimpleBullet.instance()
+	self.icon = preload("res://bullets/tracer_bullet/tracer_bullet_sprite.tscn")
+	var TracerBullet = preload('res://bullets/tracer_bullet.tscn')
+	var b = TracerBullet.instance()
 	var pl = action_handler.get_parent()
-	b.set_script(preload('res://bullets/tracer_bullet.gd'))
 	b.set_pos(pl.get_pos() + Vector2(0, -20))
 	b.speed = pl.get_look_dir().normalized() * 400
 	pl.get_parent().add_child(b)
