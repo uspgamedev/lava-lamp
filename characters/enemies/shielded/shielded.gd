100119bd0a99e4167ce5f43629792bed4dc81e5c
Kazuo256 <kazuo256@gmail.com>
2017-12-03 16:50:04 -0200

    Shielded bot sprite

---
+++ characters/enemies/shielded/shielded.gd
@@ -0,0 +1,16 @@
+extends "res://characters/enemies/enemy.gd"
+const DIR = preload("res://characters/player/input/directions.gd")
+
+onready var anim = get_node("Sprite/AnimationPlayer")
+
+const DIR_ANIMS = [
+	"up", "right", "down", "left"
+]
+
+var last_dir = -1
+
+func _fixed_process(delta):
+	var dir = self.get_look_dir_value()
+	if dir != self.last_dir:
+		anim.set_current_animation(DIR_ANIMS[dir])
+		last_dir = dir
