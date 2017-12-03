779fd0f8345a085053accd7a29ec92cbf0d77079
Yan Couto <yancouto@gmail.com>
2017-12-03 12:33:27 -0200

    Removing enemy var from ai

--- bullets/simple_bullet.gd
+++ bullets/simple_bullet.gd
@@ -17,4 +17,4 @@ func _fixed_process(delta):
 func _on_Area2D_area_enter(area):
 	if area.is_in_group('enemy_area'):
 		var enemy = area.get_parent()
-		enemy.ai.hit_by_bullet(enemy, self)
+		enemy.ai.hit_by_bullet(self)
