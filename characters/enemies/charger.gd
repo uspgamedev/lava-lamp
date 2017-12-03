c345bb3628f4e31fdfd2e6ac1a6026988b648839
Kazuo256 <kazuo256@gmail.com>
2017-12-03 19:23:52 -0200

    Charger sprite

--- characters/enemies/charger.gd
+++ characters/enemies/charger.gd
@@ -1,4 +1,27 @@
 extends 'enemy.gd'
 
+const DIR = preload("res://characters/player/input/directions.gd")
+
+onready var anim = get_node("Sprite/AnimationPlayer")
+
+const DIR_ANIMS = [
+	"up", "down", "down", "down"
+]
+
+var last_dir = -1
+
 func _ready():
 	hp = 10
+
+func _fixed_process(delta):
+	var dir = self.get_look_dir_value()
+	if dir != self.last_dir:
+		var append = ""
+		if self.speed.length_squared() > 1:
+			append = "_moving"
+		else:
+			dir = last_dir
+		anim.set_current_animation(DIR_ANIMS[dir] + append)
+		last_dir = dir
+
+	
