e2caf7a419a2052be3dc2fa3d91e6779030c503c
Kazuo256 <kazuo256@gmail.com>
2017-12-04 19:40:21 -0200

    Ghost

--- effects/sound/sfx.gd
+++ effects/sound/sfx.gd
@@ -2,7 +2,12 @@ extends SamplePlayer2D
 
 const SAMPLE = preload("res://effects/sound/sample.gd")
 
+export var autoplay = false
+export(String) var which = ""
+
 func _ready():
 	for sample_node in self.get_children():
 		if sample_node.get_script() == SAMPLE:
 			get_sample_library().add_sample(sample_node.get_name(), sample_node.sample)
+	if self.autoplay:
+		play(self.which)
