923ec9100ca6e148548291baa6bb575c75759748
Kazuo256 <kazuo256@gmail.com>
2017-12-04 15:18:28 -0200

    BUUURRRRNNNN

--- area_effects/flamethrower.gd
+++ area_effects/flamethrower.gd
@@ -4,26 +4,35 @@ onready var timer = get_node('Timer')
 onready var hit_timer = get_node('HitTimer')
 onready var pl = get_node('../Player')
 onready var input = get_node('/root/input')
+onready var sprite = get_node('Sprite')
+onready var area = get_node('Area2D')
 
 signal finish
 
 var enemyList = []
 var hold_key
+var angle = 0
 
 func _ready():
 	damage = 1
 	timer.connect('timeout', self, '_finish')
 	timer.start()
+	self.sprite.set_emitting(true)
 	set_fixed_process(true)
 	set_process_unhandled_key_input(true)
+	self._set_rot(self.angle)
+
+func _set_rot(angle):
+	sprite.set_rot(angle)
+	area.set_rot(angle)
 
 func _unhandled_key_input(ev):
 	if ev.scancode == hold_key and not ev.pressed:
 		_finish()
 
 func _fixed_process(delta):
-	self.set_pos(pl.get_pos() + Vector2(0, -20))
-	self.set_rot(pl.get_look_dir().angle())
+	self.set_pos(pl.get_pos() + 20*pl.get_look_vec())
+	self._set_rot(pl.get_look_dir().angle())
 
 func _finish():
 	set_process_unhandled_key_input(false)
