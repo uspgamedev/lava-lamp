e5f7afde50f5984e5296c14eaa89a58e28bd01b9
Kazuo256 <kazuo256@gmail.com>
2017-12-04 20:27:50 -0200

    Bouncer sfx

--- characters/enemies/bouncer/bouncer.gd
+++ characters/enemies/bouncer/bouncer.gd
@@ -1,8 +1,10 @@
 extends "res://characters/enemies/enemy.gd"
 
 onready var anim = get_node("Sprite/AnimationPlayer")
+onready var sfx = get_node('SFX')
 
 func _bounce():
+	sfx.play('Homerun')
 	anim.set_current_animation("strike")
 	yield(anim, "finished")
 	anim.set_current_animation("move")
