c235e4d19f90071925ad8b09bf2097f96138f43c
Kazuo256 <kazuo256@gmail.com>
2017-12-04 15:27:46 -0200

    Icons

--- actions/create_flamethrower.gd
+++ actions/create_flamethrower.gd
@@ -3,6 +3,7 @@ extends 'base_action.gd'
 func _init():
 	cooldown_time = 5
 	name = 'flamethrower'
+	self.icon = preload('res://effects/fire/icon.tscn')
 
 func activate(action_handler, key):
 	var Flamethrower = preload('res://area_effects/flamethrower.tscn')
