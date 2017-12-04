12224bff1dca0382c48b36e20395ab9980f5f2a2
IceMage144 <jgabriel.basi@hotmail.com>
2017-12-04 04:05:19 -0200

    Add fire while walking animation

--- characters/player/player_animation.gd
+++ characters/player/player_animation.gd
@@ -17,28 +17,36 @@ func _fixed_process(delta):
 	var anim
 		
 	if dir == DIR.UP or dir == DIR.UP_LEFT or dir == DIR.UP_RIGHT:
-		if shooting:
+		if moving and shooting:
+			anim = "Fire_Walk_Up"
+		elif shooting:
 			anim = "Fire_Up"
 		elif moving:
 			anim = "Walk_Up"
 		else:
 			anim = "Idle_Up"
 	elif dir == DIR.DOWN or dir == DIR.DOWN_LEFT or dir == DIR.DOWN_RIGHT:
-		if shooting:
+		if moving and shooting:
+			anim = "Fire_Walk_Down"
+		elif shooting:
 			anim = "Fire_Down"
 		elif moving:
 			anim = "Walk_Down"
 		else:
 			anim = "Idle_Down"
 	elif dir == DIR.LEFT:
-		if shooting:
+		if moving and shooting:
+			anim = "Fire_Walk_Left"
+		elif shooting:
 			anim = "Fire_Left"
 		elif moving:
 			anim = "Walk_Left"
 		else:
 			anim = "Idle_Left"
 	elif dir == DIR.RIGHT:
-		if shooting:
+		if shooting and moving:
+			anim = "Fire_Walk_Right"
+		elif shooting:
 			anim = "Fire_Right"
 		elif moving:
 			anim = "Walk_Right"
