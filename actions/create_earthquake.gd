2ebc34be354fa2e15ad9f31831ec325eba198465
Yan Couto <yancouto@gmail.com>
2017-12-05 15:37:32 -0200

    Fixing storm icon

--- actions/create_earthquake.gd
+++ actions/create_earthquake.gd
@@ -4,9 +4,9 @@ func _init():
 	cooldown_time = 25
 	name = "earthquake"
 	auto_play = true
+	self.icon = preload("res://effects/lightning/sprite.tscn")
 
 func activate(action_handler, key):
-	self.icon = preload("res://effects/lightning/sprite.tscn")
 	var Earthquake = preload('res://area_effects/earthquake.tscn')
 	var b = Earthquake.instance()
 	var pl = action_handler.get_parent()
