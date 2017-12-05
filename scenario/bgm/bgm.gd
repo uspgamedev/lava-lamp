68be76a6f68db09edc3b303652461faa500f834f
Victor Domiciano <victor.dm3@gmail.com>
2017-12-04 23:27:37 -0200

    Add fade when changing interlude and action mode

--- scenario/bgm/bgm.gd
+++ scenario/bgm/bgm.gd
@@ -3,6 +3,8 @@ extends Node
 onready var action = get_node("Action")
 onready var interlude = get_node("Interlude")
 onready var base = get_node("Base")
+onready var tween1 = get_node('Tween1')
+onready var tween2 = get_node('Tween2')
 
 onready var opening = get_node("Opening")
 
@@ -12,12 +14,16 @@ func _action_mode():
 		action.play()
 		interlude.play()
 		base.play()
-	action.set_volume(1)
-	interlude.set_volume(0)
+	tween1.interpolate_method(interlude, 'set_volume', interlude.get_volume(), 0, 0.1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween2.interpolate_method(action, 'set_volume', action.get_volume(), 1, 0.1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween1.start()
+	tween2.start()
 
 func _interlude_mode():
-	action.set_volume(0)
-	interlude.set_volume(1)
+	tween1.interpolate_method(action, 'set_volume', action.get_volume(), 0, 0.1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween2.interpolate_method(interlude, 'set_volume', interlude.get_volume(), 1, 0.1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween1.start()
+	tween2.start()
 
 func stop():
 	action.stop()
