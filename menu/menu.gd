00ce820ed5e49139f6db762c43eeb2393a8be8ad
ArthurTui <arthur.vb@gmail.com>
2018-10-03 10:56:22 -0300

    Added better funcions for main menu animations to remove duplicate code

--- menu/menu.gd
+++ menu/menu.gd
@@ -6,8 +6,11 @@ const RIGHT_POS = Vector2(1440, 440)
 const LEFT_OFFSET = Vector2(-300, 0)
 const OFFSCREEN_OFFSET = Vector2(-800, 0)
 
+enum {LEFT, RIGHT}
+
 onready var buttons = $Buttons
 onready var play_button = $Buttons/MainMenu/Play
+onready var options_button = $Buttons/MainMenu/Options
 onready var main_menu = $Buttons/MainMenu
 onready var play_menu = $Buttons/PlayMenu
 onready var options_menu = $Buttons/OptionsMenu
@@ -16,6 +19,7 @@ onready var tw = $Tween
 func _ready():
 	set_children_disability(play_menu, true)
 	set_children_disability(options_menu, true)
+	tw.start()
 
 
 func set_children_disability(menu, value):
@@ -24,31 +28,40 @@ func set_children_disability(menu, value):
 			child.disabled = value
 
 
-func _on_Play_pressed():
-	set_children_disability(main_menu, true)
+func main_menu_animation(pressed_button, from_menu, to_menu):
+	set_children_disability(from_menu, true)
 	
+	# Main menu buttons animation
 	for child in main_menu.get_children():
 		var offset = OFFSCREEN_OFFSET
-		if child == play_button:
+		if child == pressed_button:
 			offset = LEFT_OFFSET
+		if from_menu != main_menu:
+			offset *= -1
 		
 		tw.interpolate_property(child, "rect_position", child.rect_position,
 			child.rect_position + offset, TWEEN_DUR, Tween.TRANS_QUAD,
 			Tween.EASE_IN_OUT)
 	
-	tw.interpolate_property(play_menu, "rect_position", play_menu.rect_position,
-		CENTER_POS, TWEEN_DUR, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
-#	tw.interpolate_property(buttons, "rect_position", buttons.rect_position,
-#		buttons.rect_position - Vector2(1280, 0), TWEEN_DUR, Tween.TRANS_QUAD,
-#		Tween.EASE_IN_OUT)
-	tw.start()
+	# Selected menu animation
+	var pos = CENTER_POS
+	var menu = to_menu
+	if from_menu != main_menu:
+		pos = RIGHT_POS
+		menu = from_menu
+	tw.interpolate_property(menu, "rect_position", menu.rect_position, pos,
+		TWEEN_DUR, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	
 	yield(tw, "tween_completed")
-	set_children_disability(play_menu, false)
+	set_children_disability(to_menu, false)
+
+
+func _on_Play_pressed():
+	main_menu_animation(play_button, main_menu, play_menu)
 
 
 func _on_Options_pressed():
-	pass # replace with function body
+	main_menu_animation(options_button, main_menu, options_menu)
 
 
 func _on_Credits_pressed():
@@ -74,27 +87,8 @@ func _on_Survival_pressed():
 
 
 func _on_Play_Back_pressed():
-	set_children_disability(play_menu, true)
-	
-	for child in main_menu.get_children():
-		var offset = -OFFSCREEN_OFFSET
-		if child == play_button:
-			offset = -LEFT_OFFSET
-		
-		tw.interpolate_property(child, "rect_position", child.rect_position,
-			child.rect_position + offset, TWEEN_DUR, Tween.TRANS_QUAD,
-			Tween.EASE_IN_OUT)
-	
-	tw.interpolate_property(play_menu, "rect_position", play_menu.rect_position,
-		RIGHT_POS, TWEEN_DUR, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
-#	tw.interpolate_property(buttons, "rect_position", buttons.rect_position,
-#		buttons.rect_position + Vector2(1280, 0), TWEEN_DUR, Tween.TRANS_QUAD,
-#		Tween.EASE_IN_OUT)
-#	tw.start()
-	
-	yield(tw, "tween_completed")
-	set_children_disability(main_menu, false)
+	main_menu_animation(play_button, play_menu, main_menu)
 
 
 func _on_Options_Back_pressed():
-	pass # replace with function body
+	main_menu_animation(options_button, options_menu, main_menu)
