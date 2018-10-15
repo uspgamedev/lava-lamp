7df0e027fd07239acf9c84b64cb5a88c106615d6
mac0342 07 <victor.dm3@gmail.com>
2018-10-15 15:56:18 -0300

    Refactor game mode script

--- menu/game_mode.gd
+++ menu/game_mode.gd
@@ -1,19 +1,6 @@
 extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+const ARCADE = 0
+const SURVIVAL = 1
 
-# 0: Arcade
-# 1: Survival
-var mode = 0
-
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+var mode = ARCADE
